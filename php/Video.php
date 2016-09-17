099faae29eb536ed36d7afda1fde10ad4dbbd005
George Craig <georgeacraig@gmail.com>
2016-09-17 14:41:04 -0500

    initial php instance

---
+++ php/Video.php
@@ -0,0 +1,19 @@
+<?php
+
+class Video {
+
+    private $id;
+    private $name;
+    private $url;
+    private $url_hash;
+    private $hash_type;
+    private $mime_type;
+    private $codec;
+    private $view_ctr;
+    private $year;
+
+}
+
+?>
+
+
